5716074587195791a8c22d5da33fef7b5d90c0c6
Michael Horn <michael-horn@northwestern.edu>
2012-02-26 16:23:06 -0600

    Now subclasses Robot.java

--- src/tidal/tern/Roberto.java
+++ src/tidal/tern/Roberto.java
@@ -1,5 +1,5 @@
 /*
- * @(#) CompileException.java
+ * @(#) Roberto.java
  * 
  * Tern Tangible Programming Language
  * Copyright (c) 2011 Michael S. Horn
@@ -24,9 +24,6 @@
  */
 package tidal.tern;
 
-import android.os.Handler;
-import android.os.Message;
-import android.view.View;
 import android.view.MotionEvent;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -39,16 +36,11 @@ import android.util.Log;
 import android.content.Context;
 import android.content.res.Resources;
 
-import tidal.tern.rt.Debugger;
-import tidal.tern.rt.Process;
-
 
 /**
- * This is an "robot" interface file. The interpreter calls
- * functions on this class when it runs tern code. In turn,
- * this object displays animations of Roberto on the screen.
+ * Roberto implementation of Robot
  */
-public class Roberto extends View implements Debugger {
+public class Roberto extends Robot {
    
    public static final String TAG = "Roberto";
    
@@ -57,15 +49,6 @@ public class Roberto extends View implements Debugger {
    
    /** Roberto picture to draw */
    protected Drawable pose = null;
-   
-   /** Is there a program running */
-   protected boolean running = false;
-   
-   /** Touch sensor latch */
-   protected boolean tsensor = false;
-   
-   /** Link back to the main activity */
-   protected Tern tern = null;
    
    
    public Roberto(Context context) {
@@ -78,23 +61,6 @@ public class Roberto extends View implements Debugger {
    }
    
    
-   public void setTern(Tern tern) {
-      this.tern = tern;
-   }
-   
-   
-   public boolean onTouchEvent(MotionEvent event) {
-      int action = event.getAction();
-      if (action == MotionEvent.ACTION_DOWN) {
-         this.tsensor = true;
-      } else if (action == MotionEvent.ACTION_UP) {
-         if (! running ) tern.onClick(this);
-      }
-      invalidate();
-      return true;
-   }
-
-   
    protected void onDraw(Canvas canvas) {
       int w = getWidth();
       int h = getHeight();
@@ -102,34 +68,10 @@ public class Roberto extends View implements Debugger {
       float ds;
       
       Resources res = getResources();
-      Drawable logo = res.getDrawable(R.drawable.logo);
-      Drawable button = res.getDrawable(R.drawable.play_button_up);
-
-      // clear background 
-      canvas.drawRGB(210, 210, 210);
       
-      if (!this.running) {
-
-         // draw logo
-         dw = logo.getIntrinsicWidth();
-         dh = logo.getIntrinsicHeight();
-         ds = Math.min(0.8f, 0.8f * w / dw);
-         dw *= ds;
-         dh *= ds;
-         logo.setBounds(w/2 - dw/2, 70, w/2 + dw/2, 70 + dh);
-         logo.draw(canvas);
-
-         // draw button
-         dw = button.getIntrinsicWidth();
-         dh = button.getIntrinsicHeight();
-         dx = w - dw - 10;
-         dy = h - dh - 10;
-         button.setBounds(dx, dy, dx + dw, dy + dh);
-         button.draw(canvas);
-      }
+      drawBackground(canvas);
       
-      // Draw roberto 
-      else if (this.pose != null) {
+      if (isRunning() && this.pose != null) {
          dw = pose.getIntrinsicWidth() / 3;
          dh = pose.getIntrinsicHeight() / 3;
          dx = w/2 - dw/2;
@@ -145,22 +87,6 @@ public class Roberto extends View implements Debugger {
          canvas.drawText(this.message, w/2, h - 25, font);
       }
    }
-   
-   
-/**
- * Thread safe invalidate function
- */
-   public void repaint() {
-      repaintHandler.sendEmptyMessage(0);
-   }
-   
-   
-   private Handler repaintHandler = new Handler() {
-      @Override public void handleMessage(Message msg) {
-         invalidate();
-      }
-   };
-   
    
    private void changePicture(int d) {
       Resources res = getResources();
@@ -235,31 +161,5 @@ public class Roberto extends View implements Debugger {
    public int doDance(int [] args) {
       return 0;
    }
-   
-   
-   public int getTouchSensor(int [] args) {
-      int result = tsensor ? 1 : 0;
-      tsensor = false;
-      return result;
-   }
-   
-   
-   public void processStarted(Process p) {
-      this.running = true;
-      this.tsensor = false;
-   }
-   
-   public void processStopped(Process p) {
-      this.running = false;
-      changePicture(-1);
-   }
-   
-   public void trace(Process p, String message) { }
-   
-   public void print(Process p, String message) { }
-   
-   public void error(Process p, String message) {
-      Log.i(TAG, message);
-      this.running = false;
-   }
+
 }
